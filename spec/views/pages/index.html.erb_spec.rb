87976571041b17c87675d831e299caba1ddd3d00
barsbek <mak.bekmamat@gmail.com>
2018-02-25 21:42:57 +0500

    correct spec of index view

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe "pages/index", type: :view do
   before(:each) do
     assign(:pages, [
       Page.create!(
-        :name => "Name",
+        :name => "Name_1",
         :title => "Title",
         :text => "MyText"
       ),
       Page.create!(
-        :name => "Name",
+        :name => "Name_2",
         :title => "Title",
         :text => "MyText"
       )
@@ -18,7 +18,8 @@ RSpec.describe "pages/index", type: :view do
 
   it "renders a list of pages" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Name_1".to_s, :count => 1
+    assert_select "tr>td", :text => "Name_2".to_s, :count => 1
     assert_select "tr>td", :text => "Title".to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
   end
