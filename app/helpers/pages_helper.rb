33cf3af3bef2a7425a0f8dbdb08349f3352e1cc0
barsbek <mak.bekmamat@gmail.com>
2018-02-26 12:14:27 +0500

    change argument's name

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
-  def html_to_markdown(html)
-    html.to_s
+  def html_to_markdown(with_html)
+    with_html.to_s
       .gsub(/<b>(.+)<\/b>/) { "**#{$1}**" }
       .gsub(/<i>(.+)<\/i>/) { "\\\\#{$1}\\\\" }
       .gsub(/<a.+href\=https?:\/\/[^\/]+\/(\S+)>(.+)<\/a>/) do
