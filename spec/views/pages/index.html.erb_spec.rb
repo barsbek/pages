857852237e3775a3c44bbb61aae95fc97d19a040
barsbek <mak.bekmamat@gmail.com>
2018-02-26 12:14:08 +0500

    remove some specs

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -1,26 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "pages/index", type: :view do
-  before(:each) do
-    assign(:pages, [
-      Page.create!(
-        :name => "Name_1",
-        :title => "Title",
-        :text => "MyText"
-      ),
-      Page.create!(
-        :name => "Name_2",
-        :title => "Title",
-        :text => "MyText"
-      )
-    ])
-  end
-
-  it "renders a list of pages" do
-    render
-    assert_select "tr>td", :text => "Name_1".to_s, :count => 1
-    assert_select "tr>td", :text => "Name_2".to_s, :count => 1
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-  end
 end
--- spec/views/pages/show.html.erb_spec.rb
+++ spec/views/pages/show.html.erb_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "pages/show", type: :view do
-  before(:each) do
-    @page = assign(:page, Page.create!(
-      :name => "Name",
-      :title => "Title",
-      :text => "MyText"
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/MyText/)
-  end
 end
