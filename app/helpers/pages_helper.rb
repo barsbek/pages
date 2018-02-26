255bbc007a01617161e4e31444fc2a66999312a3
barsbek <mak.bekmamat@gmail.com>
2018-02-26 11:51:58 +0500

    transform markdown before saving and retrieval

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+  def html_to_markdown(html)
+    html.to_s
+      .gsub(/<b>(.+)<\/b>/) { "**#{$1}**" }
+      .gsub(/<i>(.+)<\/i>/) { "\\\\#{$1}\\\\" }
+      .gsub(/<a.+href\=https?:\/\/[^\/]+\/(\S+)>(.+)<\/a>/) do
+        "((#{$1}[#{$2}]))"
+      end
+  end
 end
