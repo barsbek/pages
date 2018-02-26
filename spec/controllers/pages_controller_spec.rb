0124904029ca57af2317e2a88b138d01ca4bcca3
barsbek <mak.bekmamat@gmail.com>
2018-02-26 12:14:47 +0500

    remove all pages before controller's specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  before(:all) do
+    Page.destroy_all
+  end
 
   let(:valid_attributes) {
     { name: 'name1', title: 'title1', text: 'some_text' }
