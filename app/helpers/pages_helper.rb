bc6ac125f000655c252752f0fbf2d50f926ac462
barsbek <mak.bekmamat@gmail.com>
2018-02-28 14:51:04 +0500

    test markdown_to_html helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,13 +8,13 @@ module PagesHelper
       end
   end
 
-  def markdown_to_html(val, url)
+  def markdown_to_html(val, url=nil)
     val.to_s
-      .gsub(/\*{2}([^\*{2}]+)\*{2}/) { "<b>#{$1}</b>" }
-      .gsub(/\\{2}([^\\{2}]+)\\{2}/) { "<i>#{$1}</i>" }
-      .gsub(/\({2}([^\[]+)\[(.+)\]\){2}/) do
+      .gsub(/\*{2}([^\*][^\*]+)\*{2}/) { "<b>#{$1}</b>" }
+      .gsub(/\\{2}([^\\][^\\]+)\\{2}/) { "<i>#{$1}</i>" }
+      .gsub(/\({2}([^\[]+)\[([^\]][^\)][^\)]+)\]\){2}/) do
         href = url ? "#{url}/#{$1}" : $1
-        "<a href=#{href}/#{$1}>#{$2}</a>"
+        "<a href=#{href}>#{$2}</a>"
       end
   end
 end
--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -1,14 +1,71 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the PagesHelper. For example:
-#
-# describe PagesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe PagesHelper, type: :helper do
+  describe "#markdown_to_html" do
+    context "markdown bold" do
+      let(:examples) {{
+        "**word**" =>
+          "<b>word</b>",
+
+        "text **with** multiple **occurences**" =>
+          "text <b>with</b> multiple <b>occurences</b>",
+
+        "bold in \\\\**italic**\\\\" =>
+          "bold in <i><b>italic</b></i>",
+
+        "bold ((in[**link**]))" =>
+          "bold <a href=in><b>link</b></a>",
+      }}
+
+      it "transforms **string** into <b>string</b>" do
+        examples.each do |original, result|
+          expect(markdown_to_html(original)).to eql(result)
+        end
+      end
+    end
+
+    context "markdown italic" do
+      let(:examples) {{
+        "\\\\word\\\\" => 
+          "<i>word</i>",
+
+        "text \\\\with\\\\ multiple \\\\occurences\\\\" => 
+          "text <i>with</i> multiple <i>occurences</i>",
+
+        "italic in **\\\\bold\\\\** string" =>
+          "italic in <b><i>bold</i></b> string",
+
+        "italic ((p1/p2/p3[\\\\link\\\\])) string" =>
+          "italic <a href=p1/p2/p3><i>link</i></a> string",
+      }}
+
+      it "transforms \\\\string\\\\ into <i>string</i>" do
+        examples.each do |original, result|
+          expect(markdown_to_html(original)).to eql(result)
+        end
+      end
+    end
+
+    context "markdown link" do
+      let(:examples) {{
+        "((some/url/with[text]))" => 
+          "<a href=some/url/with>text</a>",
+
+        "text with ((p1/p2/p3[string])) multiple ((url1/with1[occurences]))" => 
+          "text with <a href=p1/p2/p3>string</a> multiple <a href=url1/with1>occurences</a>",
+
+        "link in **((p1/p2/p3[bold]))** string" =>
+          "link in <b><a href=p1/p2/p3>bold</a></b> string",
+
+        "link in \\\\((p1/p2/p3[italic]))\\\\ string" =>
+          "link in <i><a href=p1/p2/p3>italic</a></i> string",
+      }}
+
+      it "transforms ((path[string])) into <a href=path>string</a>" do
+        examples.each do |original, result|
+          expect(markdown_to_html(original)).to eql(result)
+        end
+      end
+    end
+  end
 end
