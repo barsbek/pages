71aca1a6316a0aee3211f376e31a7914abb33bb6
barsbek <mak.bekmamat@gmail.com>
2018-02-28 15:03:03 +0500

    add base url into to test link's transformation

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PagesHelper, type: :helper do
   describe "#markdown_to_html" do
+
+    let(:base_url) { "http://google.com" }
+
     context "markdown bold" do
       let(:examples) {{
         "**word**" =>
@@ -14,12 +17,12 @@ RSpec.describe PagesHelper, type: :helper do
           "bold in <i><b>italic</b></i>",
 
         "bold ((in[**link**]))" =>
-          "bold <a href=in><b>link</b></a>",
+          "bold <a href=#{base_url}/in><b>link</b></a>",
       }}
 
       it "transforms **string** into <b>string</b>" do
         examples.each do |original, result|
-          expect(markdown_to_html(original)).to eql(result)
+          expect(markdown_to_html(original, base_url)).to eql(result)
         end
       end
     end
@@ -36,12 +39,12 @@ RSpec.describe PagesHelper, type: :helper do
           "italic in <b><i>bold</i></b> string",
 
         "italic ((p1/p2/p3[\\\\link\\\\])) string" =>
-          "italic <a href=p1/p2/p3><i>link</i></a> string",
+          "italic <a href=#{base_url}/p1/p2/p3><i>link</i></a> string",
       }}
 
       it "transforms \\\\string\\\\ into <i>string</i>" do
         examples.each do |original, result|
-          expect(markdown_to_html(original)).to eql(result)
+          expect(markdown_to_html(original, base_url)).to eql(result)
         end
       end
     end
@@ -49,21 +52,21 @@ RSpec.describe PagesHelper, type: :helper do
     context "markdown link" do
       let(:examples) {{
         "((some/url/with[text]))" => 
-          "<a href=some/url/with>text</a>",
+          "<a href=#{base_url}/some/url/with>text</a>",
 
         "text with ((p1/p2/p3[string])) multiple ((url1/with1[occurences]))" => 
-          "text with <a href=p1/p2/p3>string</a> multiple <a href=url1/with1>occurences</a>",
+          "text with <a href=#{base_url}/p1/p2/p3>string</a> multiple <a href=#{base_url}/url1/with1>occurences</a>",
 
         "link in **((p1/p2/p3[bold]))** string" =>
-          "link in <b><a href=p1/p2/p3>bold</a></b> string",
+          "link in <b><a href=#{base_url}/p1/p2/p3>bold</a></b> string",
 
         "link in \\\\((p1/p2/p3[italic]))\\\\ string" =>
-          "link in <i><a href=p1/p2/p3>italic</a></i> string",
+          "link in <i><a href=#{base_url}/p1/p2/p3>italic</a></i> string",
       }}
 
       it "transforms ((path[string])) into <a href=path>string</a>" do
-        examples.each do |original, result|
-          expect(markdown_to_html(original)).to eql(result)
+        examples.each do |markdown, html|
+          expect(markdown_to_html(markdown, base_url)).to eql(html)
         end
       end
     end
